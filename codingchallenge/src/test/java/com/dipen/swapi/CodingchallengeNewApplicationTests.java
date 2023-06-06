f9e742e05b749d378a207a014c5c67ab51566b6d
dipen1227 <81390178+dipen1227@users.noreply.github.com>
2023-06-06 12:23:56 -0500

    Add files via upload

---
+++ codingchallenge/src/test/java/com/dipen/swapi/CodingchallengeNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipen.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingchallengeNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
